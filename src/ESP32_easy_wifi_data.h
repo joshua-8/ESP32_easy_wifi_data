a9791321b7e0432fa3113dbf95b3002dafb34e9e
joshua-8 <59814881+joshua-8@users.noreply.github.com>
2021-05-06 21:11:43 -0700

    commented, time for testing

--- src/ESP32_easy_wifi_data.h
+++ src/ESP32_easy_wifi_data.h
@@ -4,26 +4,31 @@
 #include <WiFi.h>
 #include <WiFiUdp.h>
 namespace EWD {
-const char* routerName = "networkName";
-const char* routerPass = "networkPass";
-const char* APPass = "RCMpassword";
-int port = 25210;
+char* routerName = " ";
+char* routerPass = " ";
+char* APPass = "password";
+int wifiPort = 25210;
 int APPort = 25210;
-const boolean connectToNetwork = false; //true=try to connect to router  false=go straight to hotspot mode
-const boolean wifiRestartNotHotspot = true; //when connection issue, true=retry connection to router  false=fall back to hotspot
-const int SIGNAL_LOSS_TIMEOUT = 1000; //disable if no signal after this many milliseconds
-const boolean debugPrint = true;
-unsigned long lastMessageTimeMillis = 0;
+boolean connectToNetwork = false; //true=try to connect to router  false=go straight to hotspot mode
+boolean wifiRestartNotHotspot = false; //when connection issue, true=retry connection to router  false=fall back to hotspot
+int signalLossTimeout = 1000; //disable if no signal after this many milliseconds
+boolean debugPrint = true;
+
+#ifndef EWDmaxWifiSendBufSize
+#define EWDmaxWifiSendBufSize 32
+#endif
+#ifndef EWDmaxWifiRecvBufSize
+#define EWDmaxWifiRecvBufSize 32
+#endif
 
 namespace {
+    unsigned long lastMessageTimeMillis = 0;
     char APName[9];
     WiFiUDP udp;
-    const int maxWifiSendBufSize = 100;
-    const int maxWifiRecvBufSize = 100;
     boolean wifiConnected = false;
     boolean receivedNewData = false;
-    byte recvdData[maxWifiRecvBufSize] = { 0 };
-    byte dataToSend[maxWifiSendBufSize] = { 0 };
+    byte recvdData[EWDmaxWifiRecvBufSize] = { 0 };
+    byte dataToSend[EWDmaxWifiSendBufSize] = { 0 };
     int numBytesToSend = 0;
     int wifiArrayCounter = 0;
     IPAddress wifiIPLock = IPAddress(0, 0, 0, 0);
@@ -43,10 +48,10 @@ namespace {
             if (debugPrint) {
                 Serial.print("########## wifi connected! IP address: ");
                 Serial.print(WiFi.localIP());
-                Serial.print(" port: ");
-                Serial.println(port);
+                Serial.print(" wifiPort: ");
+                Serial.println(wifiPort);
             }
-            udp.begin(port);
+            udp.begin(wifiPort);
             wifiConnected = true;
             break;
         case SYSTEM_EVENT_AP_START:
@@ -73,25 +78,49 @@ namespace {
     }
 };
 
+unsigned long millisSinceMessage()
+{
+    return millis() - lastMessageTimeMillis;
+}
+bool notTimedOut()
+{
+    return millis() - lastMessageTimeMillis < signalLossTimeout;
+}
+bool newData()
+{
+    return receivedNewData;
+}
+
 void setupWifi(void (*_recvCP)(void), void (*_sendCP)(void))
 {
     sendCallback = _sendCP;
     recieveCallback = _recvCP;
     wifiConnected = false;
+    delay(1000);
     WiFi.disconnect(true);
-    sprintf(APName, "RCM%05d", port); // create SSID
+    sprintf(APName, "ESP%05d", wifiPort); // create SSID
     delay(1000);
     WiFi.onEvent(wifiEvent);
     if (connectToNetwork) {
         WiFi.mode(WIFI_STA);
-        WiFi.begin(routerName, routerPass);
+        if (strcmp(routerPass, "-open-network-")) {
+            WiFi.begin(routerName);
+        } else {
+            WiFi.begin(routerName, routerPass);
+        }
+        delay(2000);
+        for (int i = 0; i < 10; i++) {
+            if (wifiConnected) {
+                i = 10;
+            }
+            delay(1000);
+        }
     }
-    delay(3000);
     if (!wifiConnected) {
         if (debugPrint)
             Serial.println("########## connection to router failed/skipped");
         WiFi.disconnect(true);
-        if (wifiRestartNotHotspot) {
+        if (wifiRestartNotHotspot && connectToNetwork) {
             ESP.restart();
         }
         WiFi.mode(WIFI_AP);
@@ -112,7 +141,7 @@ void setupWifi(void (*_recvCP)(void), void (*_sendCP)(void))
         Serial.println("########## wifi malfunction, try reboot");
     }
     if (debugPrint)
-        Serial.println("########## starting main loop");
+        Serial.println("########## wifi setup complete.");
 }
 
 void runWifiCommunication()
@@ -121,23 +150,24 @@ void runWifiCommunication()
     if (udp.remoteIP() != IPAddress(0, 0, 0, 0) && wifiIPLock == IPAddress(0, 0, 0, 0)) {
         wifiIPLock = udp.remoteIP();
     }
-    if (millis() - lastMessageTimeMillis > SIGNAL_LOSS_TIMEOUT) {
+    if (millis() - lastMessageTimeMillis > signalLossTimeout) {
         wifiIPLock = IPAddress(0, 0, 0, 0);
     }
     receivedNewData = false;
-    if (packetSize) { //got a message
-        udp.read(recvdData, maxWifiRecvBufSize);
+    if (packetSize && packetSize <= EWDmaxWifiRecvBufSize) { //got a message
+        udp.read(recvdData, EWDmaxWifiRecvBufSize);
+        udp.flush();
         if (udp.remoteIP() == wifiIPLock || wifiIPLock == IPAddress(0, 0, 0, 0)) {
             receivedNewData = true;
             lastMessageTimeMillis = millis();
-            for (int i = 0; i < maxWifiRecvBufSize; i++) {
+            for (int i = 0; i < EWDmaxWifiRecvBufSize; i++) {
                 recvdData[i] = (byte)((int)(256 + recvdData[i]) % 256);
             }
             wifiArrayCounter = 0;
             recieveCallback();
             wifiArrayCounter = 0;
             sendCallback();
-            numBytesToSend = wifiArrayCounter;
+            numBytesToSend = min(wifiArrayCounter, EWDmaxWifiSendBufSize);
             udp.beginPacket();
             for (byte i = 0; i < numBytesToSend; i++) {
                 udp.write(dataToSend[i]);
@@ -149,27 +179,27 @@ void runWifiCommunication()
 
 boolean recvBl()
 { // return boolean at pos position in recvdData
-    byte msg = recvdData[wifiArrayCounter];
+    byte msg = recvdData[min(wifiArrayCounter, EWDmaxWifiRecvBufSize - 1)];
     wifiArrayCounter++; // increment the counter for the next value
     return (msg == 1);
 }
 
 byte recvBy()
 { // return byte at pos position in recvdData
-    byte msg = recvdData[wifiArrayCounter];
+    byte msg = recvdData[min(wifiArrayCounter, EWDmaxWifiRecvBufSize - 1)];
     wifiArrayCounter++; // increment the counter for the next value
     return msg;
 }
 
 int recvIn()
-{ // return int from four bytes starting at pos position in recvdData
-    union { // this lets us translate between two variable types (equal size, but one's four bytes in an array, and one's a four byte int)  Reference for unions: https:// www.mcgurrin.info/robots/127/
+{ // return int from four bytes starting at pos position in recvdData (esp32s use 4 byte ints)
+    union { // this lets us translate between two variable types (equal size, but one's four bytes in an array, and one's a four byte int)  Reference for unions: https://www.mcgurrin.info/robots/127/
         byte b[4];
         int v;
     } d; // d is the union, d.b acceses the byte array, d.v acceses the int
 
     for (int i = 0; i < 4; i++) {
-        d.b[i] = recvdData[wifiArrayCounter];
+        d.b[i] = recvdData[min(wifiArrayCounter, EWDmaxWifiRecvBufSize - 1)];
         wifiArrayCounter++;
     }
 
@@ -178,13 +208,13 @@ int recvIn()
 
 float recvFl()
 { // return float from 4 bytes starting at pos position in recvdData
-    union { // this lets us translate between two variable types (equal size, but one's 4 bytes in an array, and one's a 4 byte float) Reference for unions: https:// www.mcgurrin.info/robots/127/
+    union { // this lets us translate between two variable types (equal size, but one's 4 bytes in an array, and one's a 4 byte float) Reference for unions: https://www.mcgurrin.info/robots/127/
         byte b[4];
         float v;
     } d; // d is the union, d.b acceses the byte array, d.v acceses the float
 
     for (int i = 0; i < 4; i++) {
-        d.b[i] = recvdData[wifiArrayCounter];
+        d.b[i] = recvdData[min(wifiArrayCounter, EWDmaxWifiRecvBufSize - 1)];
         wifiArrayCounter++;
     }
 
@@ -193,34 +223,34 @@ float recvFl()
 
 void sendBl(boolean msg)
 { // add a boolean to the tosendData array
-    dataToSend[wifiArrayCounter] = msg;
+    dataToSend[min(wifiArrayCounter, EWDmaxWifiSendBufSize - 1)] = msg;
     wifiArrayCounter++;
 }
 
 void sendBy(byte msg)
 { // add a byte to the tosendData array
-    dataToSend[wifiArrayCounter] = msg;
+    dataToSend[min(wifiArrayCounter, EWDmaxWifiSendBufSize - 1)] = msg;
     wifiArrayCounter++;
 }
 
 void sendIn(int msg)
-{ // add an int to the tosendData array (four bytes)
+{ // add an int to the tosendData array (four bytes, esp32s use 4 byte ints)
     union {
         byte b[4];
         int v;
-    } d; // d is the union, d.b acceses the byte array, d.v acceses the int
+    } d; // d is the union, d.b acceses the byte array, d.v acceses the int (equal size, but one's 4 bytes in an array, and one's a 4 byte int Reference for unions: https://www.mcgurrin.info/robots/127/
 
     d.v = msg; // put the value into the union as an int
 
     for (int i = 0; i < 4; i++) {
-        dataToSend[wifiArrayCounter] = d.b[i];
+        dataToSend[min(wifiArrayCounter, EWDmaxWifiSendBufSize - 1)] = d.b[i];
         wifiArrayCounter++;
     }
 }
 
 void sendFl(float msg)
 { // add a float to the tosendData array (four bytes)
-    union { // this lets us translate between two variables (equal size, but one's 4 bytes in an array, and one's a 4 byte float Reference for unions: https:// www.mcgurrin.info/robots/127/
+    union { // this lets us translate between two variables (equal size, but one's 4 bytes in an array, and one's a 4 byte float Reference for unions: https://www.mcgurrin.info/robots/127/
         byte b[4];
         float v;
     } d; // d is the union, d.b acceses the byte array, d.v acceses the float
@@ -228,7 +258,7 @@ void sendFl(float msg)
     d.v = msg;
 
     for (int i = 0; i < 4; i++) {
-        dataToSend[wifiArrayCounter] = d.b[i];
+        dataToSend[min(wifiArrayCounter, EWDmaxWifiSendBufSize - 1)] = d.b[i];
         wifiArrayCounter++;
     }
 }
--- src/main.ino
+++ src/main.ino
@@ -1,20 +1,42 @@
 #include <Arduino.h>
+
+#define EWDmaxWifiSendBufSize 8 //how many bytes will your outgoing message be? (boolean=1,byte=1,int=4,float=4) (comment this line to use the default of 32)
+#define EWDmaxWifiRecvBufSize 8 //how many bytes will your incoming message be? (boolean=1,byte=1,int=4,float=4) (comment this line to use the default of 32)
 #include <ESP32_easy_wifi_data.h>
+
 void setup()
 {
-    EWD::setupWifi(EWDdataRecieveCallback, EWDdataSendCallback);
-    // put your setup code here, to run once:
+    Serial.begin(9600);
+    EWD::connectToNetwork = true; //true=try to connect to router  false=go straight to hotspot mode (default: false)
+    EWD::wifiRestartNotHotspot = false; //when connection issue, true=retry connection to router  false=fall back to hotspot (default: false)
+    EWD::signalLossTimeout = 1000; //disable if no signal after this many milliseconds (default: 1000)
+    EWD::routerName = "chicken"; //name of the wifi network you want to connect to
+    EWD::routerPass = "bawkbawk"; //password for your wifi network (enter "-open-network-" if the network has no password) (default: -open-network-)
+    EWD::APPass = "mypassword"; //password for network created by esp32 if it goes into hotspot mode (default: password)
+    EWD::wifiPort = 25210; //what port the esp32 communicates on if connected to a wifi network (default: 25210)
+    EWD::APPort = 25210; //what port the esp32 communicates on if it makes its own network (default 25210)
+    EWD::debugPrint = true; //print ip address and other information to the serial monitor (default: true)
+    EWD::setupWifi(dataRecieveCallback, dataSendCallback); //this function connects to wifi, it may take up to about 10 seconds
 }
 
 void loop()
 {
-    EWD::runWifiCommunication();
-    // put your main code here, to run repeatedly:
+    EWD::runWifiCommunication(); //call in loop, as often as possible in order to stay connected
+    EWD::millisSinceMessage(); //returns how many milliseconds it has been since a message was last recieved
+    EWD::notTimedOut(); //if millisSinceMessage<signalLossTimeout, use to disable your robot if it loses connection
+    EWD::newData(); //new data was recieved this loop
 }
-void EWDdataRecieveCallback()
+void dataRecieveCallback()
 {
+    EWD::recvBl(); //recieve a boolean
+    EWD::recvBy(); //recieve a byte
+    EWD::recvIn(); //recieve an int
+    EWD::recvFl(); //recieve a float
 }
-void EWDdataSendCallback()
+void dataSendCallback()
 {
-    Serial.println("SEND CALLBACK !!!!!!!!!!!!!!!");
+    EWD::sendBl(true); //send a boolean
+    EWD::sendBy(180); //send a byte
+    EWD::sendIn(654321); //send an int
+    EWD::sendFl(9.8); //send a float
 }
